32adc92f1087f6fc8f91b3a986ab2dfe2fc49012
Emanuel Jimenez <emanuel.jimenez+DFIN@dfinsolutions.com>
2023-09-12 12:43:55 -0300

    Update WeatherForecastController.cs

--- WebApplicationA/Controllers/WeatherForecastController.cs
+++ WebApplicationA/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplicationA.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezingss", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
