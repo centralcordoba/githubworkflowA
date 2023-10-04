c6a835774f1100c6658bc8b66f09862ca4ef700e
Emanuel Jimenez <emanuel.jimenez+DFIN@dfinsolutions.com>
2023-10-04 16:57:01 -0300

    red

--- .github/scripts/scan_repositories.py
+++ .github/scripts/scan_repositories.py
@@ -1,60 +1,77 @@
+import os
 import requests
 import csv
-import os
 
-# Obtener el token de GitHub desde las variables de entorno
-TOKEN = os.getenv("GH_TOKEN")
+# URL base de la API de GitHub para obtener las vulnerabilidades
+VULNERABILITIES_URL = "https://api.github.com/repos/{}/vulnerability-alerts"
 
-# Verificación del token
-if TOKEN is None:
-    raise ValueError("GH_TOKEN no está definido.")
-
-# Configuración del encabezado para las solicitudes HTTP
-HEADERS = {
-    "Authorization": f"token {TOKEN}",
-    "Accept": "application/vnd.github.vixen-preview+json"
-}
-
-# Repositorios a verificar
+# Repositorios a analizar
 REPOSITORIES = [
     "centralcordoba/githubworkflowA",
     "centralcordoba/MarvelAngular",
-    # Añadir más repositorios según sea necesario
+    # ... otros repositorios ...
 ]
 
-# Inicializar el archivo CSV
-with open('vulnerabilities.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Repository", "Vulnerability Name", "Severity", "Package Name", "Package Version"])
-    
-    # Loop a través de cada repositorio
-    for repo in REPOSITORIES:
-        print(f"Fetching vulnerabilities for {repo}...")
-        url = f"https://api.github.com/repos/{repo}/vulnerability-alerts"
-        
-        # Realizar la solicitud HTTP
-        response = requests.get(url, headers=HEADERS)
-        
-        # Verificación de la respuesta
-        if response.status_code != 200:
-            print(f"Status Code for {repo}: {response.status_code}")
-            print("Failed to fetch vulnerabilities. Response Headers:")
-            print(response.headers)
-            print("Response Body:")
-            print(response.text)
-            continue
-        
-        # Procesar la respuesta JSON
-        vulnerabilities = response.json()
-        for vuln in vulnerabilities:
-            writer.writerow([
-                repo,
-                vuln.get('name', ''),
-                vuln.get('severity', ''),
-                vuln.get('vulnerable_package_name', ''),
-                vuln.get('vulnerable_package_version', '')
-            ])
-        
-        print(f"Vulnerabilities for {repo} fetched and recorded.")
+# Token de acceso personal de GitHub
+TOKEN = os.getenv("GH_TOKEN")
 
-print("Script execution completed.")
+# Encabezados de la solicitud HTTP
+HEADERS = {
+    "Accept": "application/vnd.github.vixen-preview+json",
+    "Authorization": f"token {TOKEN}"
+}
+
+# Nombre del archivo CSV donde se guardarán los resultados
+CSV_FILENAME = "vulnerabilities.csv"
+
+def fetch_vulnerabilities(repo):
+    """
+    Fetch the vulnerabilities for the given repo using GitHub API.
+    
+    Parameters:
+        repo (str): The repository in the form "owner/repo".
+    
+    Returns:
+        dict: The JSON response from the API if successful, otherwise None.
+    """
+    response = requests.get(VULNERABILITIES_URL.format(repo), headers=HEADERS)
+    
+    if response.status_code == 204:
+        print(f"No vulnerabilities found for {repo}.")
+        return None
+    elif response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to fetch vulnerabilities for {repo}.")
+        print(f"Status Code for {repo}: {response.status_code}")
+        print(f"Response Headers:\n{response.headers}")
+        print(f"Response Body:\n{response.text}")
+        return None
+
+def main():
+    """
+    Main function that fetches vulnerabilities for repositories and saves them to a CSV file.
+    """
+    with open(CSV_FILENAME, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # Escribir encabezados de columna
+        writer.writerow(["Repository", "Vulnerability Name", "Severity", "Package Name", "Package Version"])
+        
+        for repo in REPOSITORIES:
+            print(f"Fetching vulnerabilities for {repo}...")
+            vulnerabilities = fetch_vulnerabilities(repo)
+            
+            # Procesar las vulnerabilidades si las hay
+            if vulnerabilities:
+                for vuln in vulnerabilities:
+                    writer.writerow([
+                        repo,
+                        vuln['security_advisory']['ghsa_id'],
+                        vuln['security_advisory']['severity'],
+                        vuln['vulnerable_package_name'],
+                        vuln['vulnerable_package_version']
+                    ])
+
+# Si este script se ejecuta como el principal, ejecutar la función principal
+if __name__ == "__main__":
+    main()
