1cbe4f364fd3d4006731afcd0ffc2d7dafd84110
Emanuel Jimenez <emanuel.jimenez+DFIN@dfinsolutions.com>
2023-10-05 06:36:23 -0300

    update python

--- .github/scripts/scan_repositories.py
+++ .github/scripts/scan_repositories.py
@@ -2,51 +2,63 @@ import requests
 import pandas as pd
 import os
 
-# Reemplazar con tu token de GitHub
-TOKEN = 'GH_TOKEN'
+# Import logging
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# Tu token personal de GitHub
+TOKEN = os.getenv("GH_TOKEN")
 HEADERS = {
-    'Authorization': f'token {TOKEN}',
-    'Accept': 'application/vnd.github.vixen-preview+json'
+    "Authorization": f"token {TOKEN}",
+    "Accept": "application/vnd.github.vixen-preview+json"
 }
 
-# Lista de repositorios para verificar
-REPOS = [
-    {'owner': 'centralcordoba', 'repo': 'githubworkflowA'},
-    {'owner': 'centralcordoba', 'repo': 'MarvelAngular'},
-    # Añadir más repositorios si es necesario
+# Lista de repositorios a verificar
+REPO_LIST = [
+    'centralcordoba/githubworkflowA',
+    'centralcordoba/MarvelAngular',
+    # Agrega los repositorios adicionales aquí
 ]
 
-# Lista para almacenar los datos recopilados
-data = []
-
-# Iterar sobre cada repositorio y obtener las alertas de seguridad
-for repo_info in REPOS:
-    owner = repo_info['owner']
-    repo = repo_info['repo']
-    url = f'https://api.github.com/repos/{owner}/{repo}/vulnerability-alerts'
-    
+def fetch_vulnerabilities(repo):
+    logging.info(f"Fetching vulnerabilities for {repo}...")
+    url = f"https://api.github.com/repos/{repo}/vulnerability-alerts"
     response = requests.get(url, headers=HEADERS)
-    
-    if response.status_code == 200:
-        vulnerabilities = response.json()
-        
-        for vuln in vulnerabilities:
-            data.append({
-                'Repository': f"{owner}/{repo}",
-                'Vulnerability': vuln['security_advisory']['vulnerabilities'][0]['package']['name'],
-                'Severity': vuln['security_advisory']['vulnerabilities'][0]['severity'],
-                'Updated_At': vuln['security_advisory']['updated_at'],
-                'Advisory_URL': vuln['security_advisory']['html_url']
-            })
-    elif response.status_code == 404:
-        print(f"No vulnerabilities found for {owner}/{repo}.")
-    else:
-        print(f"Failed to fetch vulnerabilities for {owner}/{repo}.")
 
-# Convertir los datos a un DataFrame y luego a un CSV
-if data:
-    df = pd.DataFrame(data)
-    df.to_csv('vulnerabilities.csv', index=False)
-    print("CSV file created successfully.")
-else:
-    print("No vulnerabilities found for all repositories.")
+    logging.info(f"Status Code for {repo}: {response.status_code}")
+    if response.status_code != 200:
+        logging.error(f"Failed to fetch vulnerabilities for {repo}.")
+        logging.error(f"Response Headers: {response.headers}")
+        logging.error(f"Response Body: {response.text}")
+        return []
+
+    vulnerabilities = response.json()
+    logging.info(f"Found {len(vulnerabilities)} vulnerabilities for {repo}.")
+    return [
+        {
+            "Repository": repo,
+            "Vulnerability Name": vuln["security_advisory"]["vulnerabilities"][0]["package"]["name"],
+            "Severity": vuln["security_advisory"]["severity"],
+            "Package Name": vuln["security_advisory"]["vulnerabilities"][0]["package"]["name"],
+            "Package Version": vuln["security_advisory"]["vulnerabilities"][0]["version"]
+        }
+        for vuln in vulnerabilities
+    ]
+
+def main():
+    all_vulnerabilities = []
+
+    for repo in REPO_LIST:
+        vulnerabilities = fetch_vulnerabilities(repo)
+        all_vulnerabilities.extend(vulnerabilities)
+
+    if all_vulnerabilities:
+        df = pd.DataFrame(all_vulnerabilities)
+        df.to_csv("vulnerabilities.csv", index=False)
+        logging.info("CSV file with vulnerabilities created successfully.")
+    else:
+        logging.info("No vulnerabilities found for all repositories.")
+
+if __name__ == "__main__":
+    main()
